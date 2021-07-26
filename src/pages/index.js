6519fa92f21972c670e5653899400f0367ad20b8
fawasam <fawasam5@gmail.com>
2021-07-26 21:39:44 +0530

    img follow text

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,51 @@
-import React from "react"
-import "../styles/home.scss"
+import React, { useEffect, useState, useRef } from "react";
+import Media from "../components/Media";
+import Title from "../components/Title";
+import "../styles/home.scss";
+import sampleData from "../utils/sampleData";
 
-export default function Home() {
-  return <div>Hello world!</div>
+// another example https://codedaily.io/tutorials/60/Create-a-useMousePosition-Hook-with-useEffect-and-useState-in-React
+const useMousePosition = () => {
+  const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 });
+
+  useEffect(() => {
+    const updateMousePosition = e => {
+      setMousePosition({ x: e.clientX, y: e.clientY });
+    };
+    window.addEventListener("mousemove", updateMousePosition);
+
+    return () => window.removeEventListener("mousemove", updateMousePosition);
+  }, []);
+
+  return mousePosition;
+};
+
+export default () => {
+  const { x, y } = useMousePosition();
+  const [activeIndex, setActiveIndex] = useState(-1);
+
+  return (
+    <div className="page-wrapper">
+      <div className="project-list">
+        {sampleData.map(({ title, mediaUrl }, index) => (
+          <Title
+            key={index}
+            title={title}
+            index={index}
+            setActiveIndex={setActiveIndex}
+          />
+        ))}
+      </div>
+      <div className="project-media">
+        {sampleData.map(({ mediaUrl, mediaType }, index) => {
+          const isActive = index === activeIndex;
+          const xPos = isActive ? x : 0;
+          const yPos = isActive ? y : 0;
+          return (
+                <Media  url={mediaUrl} x={xPos} y={yPos} active={isActive} />
+          );
+        })}
+      </div>
+    </div>
+  )
 }
